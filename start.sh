a36056df46015fdb2da5d4050ea50b6f7ebe8108
Pushkar Pande <pushkar.in@gmail.com>
2015-05-14 23:43:49 -0700

    Corrected docker image name according to build.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker run -d -p 5000:5000 --hostname=registry --name=registry \
   -v `pwd`/config.yml:/usr/local/lib/python2.7/dist-packages/config/config.yml \
-  -e "SETTINGS_FLAVOR=dev" ppande/docker-registry
+  -e "SETTINGS_FLAVOR=dev" ppande/registry-v1
