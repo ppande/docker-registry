839c08dc160c1d435c266295d51e3e466e0c68fb
Pushkar Pande <pushkar.in@gmail.com>
2015-05-19 13:32:19 -0700

    Removed commented out instructions from Dockerfile. Updated start.sh script to launch registry redis and registry frontend as well.

--- Dockerfile
+++ Dockerfile
@@ -17,10 +17,6 @@ RUN   pip install docker-registry
  
 RUN   apt-get clean && \
       rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-#RUN   locale-gen en_US.UTF-8 && dpkg-reconfigure locales
 RUN   mkdir /var/docker-registry
 ADD   config.yml /usr/local/lib/python2.7/dist-packages/config/config.yml
-#RUN   cd /usr/local/lib/python2.7/dist-packages/config && \
-#      cp config_sample.yml config.yml && \
-#      sed -i.bak 's/\/tmp\//\/var\/docker-registry\//g' config.yml
 CMD   ["gunicorn", "--access-logfile", "-", "--debug", "--max-requests", "100", "--graceful-timeout", "3600", "-t", "3600", "-k", "gevent", "-b", "0.0.0.0:5000", "-w", "4", "docker_registry.wsgi:application"]
--- start.sh
+++ start.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-docker run -d -p 5000:5000 --hostname=registry --name=registry \
+REGISTRY_LOCATION=/tmp
+
+docker pull redis:latest 
+docker pull ppande/registry-v1 
+docker pull konradkleine/docker-registry-frontend
+
+docker run -d -p 6379:6379 --name registry-redis --hostname registry-redis redis:latest && \
+docker run -d -p 5000:5000 --hostname=registry-v1 --name=registry-v1 \
+  --link registry-redis:registry-redis \
+  -e SEARCH_BACKEND=sqlalchemy \
+  -e STORAGE_PATH=/var/docker-registry \
+  -e CACHE_REDIS_HOST=registry-redis \
+  -e CACHE_REDIS_PORT=6379 \
+  -e CACHE_REDIS_DB=0 \
+  -e CACHE_LRU_REDIS_HOST=registry-redis \
+  -e CACHE_LRU_REDIS_PORT=6379 \
+  -e CACH_LRU_REDIS_DB=0 \
   -v `pwd`/config.yml:/usr/local/lib/python2.7/dist-packages/config/config.yml \
-  -e "SETTINGS_FLAVOR=dev" ppande/registry-v1
+  -v $REGISTRY_LOCATION:/var/docker-registry \
+  -e SETTINGS_FLAVOR=dev ppande/registry-v1 && \
+docker run -d --link registry-v1:registry-v1 \
+  --name registry-frontend \
+  --hostname registry-frontend \
+  -e ENV_DOCKER_REGISTRY_HOST=registry-v1 \
+  -e ENV_DOCKER_REGISTRY_PORT=5000 \
+  -p 8280:80 \
+  konradkleine/docker-registry-frontend
